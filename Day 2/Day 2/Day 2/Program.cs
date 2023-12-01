265ebbb1f944e9d1050bd10195e7eb10267ad308
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-01 18:17:51 +0530

    Add files via upload

---
+++ Day 2/Day 2/Day 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Day_2
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*for (int i = 0; i <=10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            } 
+            for (int i = 10; i >=0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            var name = "Adnan Khan";
+            for (var i = 0;i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            foreach(var character in name)
+            {
+                Console.WriteLine(character);
+            }
+            var numbers= new int[] { 1, 2, 3};
+            foreach (var character in numbers)
+            {
+                Console.WriteLine(character);
+            }
+            var j = 0;
+            while (j<=10)
+            {
+                if(j%2==0)
+                Console.WriteLine(j);
+                j++;
+            }
+            while(true)
+            {
+                Console.WriteLine("Enter Your Name: ");
+                var input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                    continue;
+                Console.WriteLine("Your Name is : "+input);
+            }
+            */
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
+
+        }
+    }
+}
