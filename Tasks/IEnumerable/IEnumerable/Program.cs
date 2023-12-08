0be6180ef8763c5caa228dcb8648e8336e678c19
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-08 18:14:06 +0530

    Add files via upload

---
+++ Tasks/IEnumerable/IEnumerable/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+namespace IEnumerable
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+
+            Console.WriteLine("IEnumerable ");
+            var employees = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name = "Bill", Salary =20000},
+                new Employee(){Id = 2, Name = "Adnan ", Salary = 10000},
+                new Employee(){Id = 3, Name = "George ", Salary = 1000},
+                new Employee(){Id = 4, Name = "John", Salary = 16000}
+            };
+            IEnumerable<Employee> salary = employees.Where(l => l.Salary > 15000).AsEnumerable<Employee>();
+            foreach (Employee employee in salary)
+            {
+                Console.WriteLine(employee.Id + " " + employee.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("IQueryable");
+      
+            IQueryable<Employee> newsalary = employees.Where(l => l.Salary > 15000).AsQueryable<Employee>();
+            foreach (Employee employee in newsalary)
+            {
+                Console.WriteLine(employee.Id + " " + employee.Name);
+            }
+        }
+    }
+}
---
+++ Tasks/VarDynamicObject/VarDynamicObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace VarDynamicObject
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var number = 10;
+            var text = "Hello, World!";
+            var flag = true;
+
+            Console.WriteLine($"var Example:");
+            Console.WriteLine($"Number: {number}, Type: {number.GetType()}");
+            Console.WriteLine();
+            Console.WriteLine($"Text: {text}, Type: {text.GetType()}");
+            Console.WriteLine();
+            Console.WriteLine($"Flag: {flag}, Type: {flag.GetType()}");
+            Console.WriteLine();
+            Console.WriteLine();
+            dynamic dynamicObject = 10;
+            Console.WriteLine($"dynamic Example:");
+            Console.WriteLine($"Dynamic Object: {dynamicObject}, Type: {dynamicObject.GetType()}");
+            Console.WriteLine();
+            dynamicObject = "Dynamic Text";
+            Console.WriteLine($"Dynamic Object: {dynamicObject}, Type: {dynamicObject.GetType()}");
+            Console.WriteLine();
+            dynamicObject = true;
+            Console.WriteLine($"Dynamic Object: {dynamicObject}, Type: {dynamicObject.GetType()}");
+        }
+
+
+    }
+}
+
