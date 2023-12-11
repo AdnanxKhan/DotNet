3186b918e01ece37db6b5ac20f78fc7754f905e4
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-11 18:21:43 +0530

    Add files via upload

---
+++ Day10/Models/CustomHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day10.Models
+{
+    public static class CustomHelper
+    {
+
+        public static IHtmlString Image(string src, string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>", src, alt));
+        }
+
+    }
+}
---
+++ Day10/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Day10.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool isEmployee { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
