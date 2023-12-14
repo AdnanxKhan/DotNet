28bb8da4d2075f2051651d7bfda5f796c7817788
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-14 18:20:48 +0530

    Add files via upload

---
+++ Day13/Day13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Day13
+{
+    public class Program
+
+    {
+        public int a;
+        public Program(int a)
+        {
+            this.a = a;
+          
+        }
+        public Program(Program abc)
+        {
+            abc.a = a;
+        }
+        public static void Main(string[] args)
+        {
+      
+            int value = new Program(1);
+            var abc = new Program(2);
+            Console.WriteLine(value);
+            Console.WriteLine(abc);
+        }
+    }
+}
---
+++ Day13/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CRUDStart.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        [Required]
+        [DisplayName("Employee ID: ")]
+        public int ID { get; set; }
+        [Required]
+        [DisplayName("Employee Name: ")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Employee Department: ")]
+        public string Department { get; set; }
+        [Required]
+        [DisplayName("Employee Salary: ")]
+        public int Salary { get; set; }
+    }
+}
