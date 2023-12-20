6cf6b1a579f6fa9c45f8ab7f2b693de7d8995f8b
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-20 18:20:18 +0530

    Add files via upload

---
+++ Day17/Migrations/202312181001304_AddMovie-force.cs
@@ -0,0 +1,26 @@
+﻿namespace MyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieforce : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        MovieId = c.Int(nullable: false, identity: true),
+                        MovieName = c.String(),
+                    })
+                .PrimaryKey(t => t.MovieId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
