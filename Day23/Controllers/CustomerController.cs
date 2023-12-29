aa9b07dd11e8ca9d7b5cff4cfed260e474d8d564
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2023-12-29 18:23:18 +0530

    Add files via upload

---
+++ Day23/Controllers/CustomerController.cs
@@ -0,0 +1,143 @@
+﻿using NewApp.Models;
+using NewApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace NewApp.Controllers
+{
+    
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            //List<Customer> customerList = _context.Customers.Include("MembershipTypes").ToList();
+            ////var customer = new Customer();  
+
+            //return View(customerList);
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            Customer customer = _context.Customers.Include("MembershipTypes").SingleOrDefault(c => c.CustomerId == id);
+
+            return View(customer);
+        }
+        public ActionResult New()
+        {
+
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewmodel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewmodel);
+        }
+        public ActionResult Create()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = new Customer(),
+                MembershipTypes = membershipTypes
+            };
+
+            return View("Create", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+
+                };
+                return View("Create", viewModel);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.CustomerId == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("Edit", viewModel);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            if (viewModel.Customer.CustomerId == null)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.SingleOrDefault(c => c.CustomerId == viewModel.Customer.CustomerId);
+                customerInDb.CustomerName = viewModel.Customer.CustomerName;
+                customerInDb.BirthDate = viewModel.Customer.BirthDate;
+                customerInDb.MembershipTypeId = viewModel.Customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = viewModel.Customer.IsSubscribedToNewsLetter;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customer");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.CustomerId == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.CustomerId == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+
+
+
+    }
+}
---
+++ Day23/Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace NewApp.Dtos
+{
+    public class MovieDto
+    {
+        [Required]
+        public int MovieId { get; set; }
+        [Required(ErrorMessage = "Movie Name is Required.")]
+
+        public string MovieName { get; set; }
+        [Required(ErrorMessage = "Genre is Required.")]
+
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "Release Date is Required.")]
+        [DisplayName("Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Date Added on is Required.")]
+
+        public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "Number in Stock is Required.")]
+
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
