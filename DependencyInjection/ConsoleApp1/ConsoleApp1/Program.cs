39fcae9f21fc8010c0bc3e5fc132e4cc495b3dfe
AdnanxKhan <152466859+AdnanxKhan@users.noreply.github.com>
2024-02-27 17:55:08 +0530

    Add files via upload

---
+++ DependencyInjection/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+public class Engine
+{
+    public void Start()
+    {
+        Console.WriteLine("Engine started.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Engine stopped.");
+    }
+}
+
+
+public class Car
+{
+    private readonly Engine _engine;
+
+
+    public Car(Engine engine)
+    {
+        _engine = engine;
+    }
+
+    public void StartCar()
+    {
+        _engine.Start();
+        Console.WriteLine("Car started.");
+    }
+
+    public void StopCar()
+    {
+        _engine.Stop();
+        Console.WriteLine("Car stopped.");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Engine engine = new Engine();
+
+       
+        Car car = new Car(engine);
+
+        
+        car.StartCar();
+        car.StopCar();
+    }
+}
